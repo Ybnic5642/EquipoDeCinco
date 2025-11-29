2c98dd08640c96a090b46fbc746871662729b844
Yess <cary090121mgtrysa9@cbtis217.edu.mx>
2025-11-28 19:41:01 -0600

    Cris,love you

---
+++ TrabajoB/TrabajoB/trabajoB.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC37
+ * Date: 28/11/2025
+ * Time: 07:14 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TrabajoB
+{
+	/// <summary>
+	/// Description of trabajoB.
+	/// </summary>
+	public partial class trabajoB : Form
+	{
+		public trabajoB()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
